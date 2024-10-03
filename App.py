69b8fb595f5ca95e1557f32f9fba1a25941d1c0f
akacj24 <131033895+akacj24@users.noreply.github.com>
2024-10-03 10:03:08 -0500

    Create App.py

---
+++ App.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+
+st.title('Reconocimiento de Dígitos escritos a mano')
+
+with st.sidebar:
+stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+
+drawing_mode = st.selectbox(
+  ("freedraw")
+)
+stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+stroke_color = st.color_picker("Pick A Color", "#00f900")
+st.write("The current color is", color)
+bg_color = st.color_picker("Pick A Background Color", "#00f900")
+
+# Create a canvas component
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    height=200,
+    width=200,
+    key="canvas",
+)
